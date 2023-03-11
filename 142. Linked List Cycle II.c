ff245209f3b1fb1d15498cfbdf548626ace81390
jnarin <jnarin@users.noreply.github.com>
2023-03-11 11:28:46 +0530

    Update 142. Linked List Cycle II.c

--- 142. Linked List Cycle II.c
+++ 142. Linked List Cycle II.c
@@ -1,3 +1,11 @@
+/* 
+Link: https://leetcode.com/problems/linked-list-cycle-ii/
+Problem: 142. Linked List Cycle II
+Approach:
+1. Two-pointer approach to determine if there is a cycle (fast and slow)
+2. When cycle is detected, increment both pointers at same interval to determine solution
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
