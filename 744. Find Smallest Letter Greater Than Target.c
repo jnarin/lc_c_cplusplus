1e514130970fab229d4e37746e1b9663d321d0d6
jnarin <jnarin@users.noreply.github.com>
2023-06-09 09:41:08 +0530

    Add files via upload

--- 744. Find Smallest Letter Greater Than Target.c
+++ 744. Find Smallest Letter Greater Than Target.c
@@ -2,19 +2,20 @@
 Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
 Problem: 744. Find Smallest Letter Greater Than Target
 */
-
 char nextGreatestLetter(char* letters, int lettersSize, char target){
-    assert(letters);
-    
-    int i;
+    int map[26] = {0}, i;
     char ans = letters[0];
-    
     for (i = 0; i < lettersSize; i++){
-        if (letters[i] > target){
-            ans = letters[i];
+        map[letters[i] - 'a']++;
+    }
+    
+    for (i = target + 1; i <= 'z'; i++){
+        if (map[i - 'a'] > 0){
+            ans = i;
             break;
         }
     }
     
     return ans;
+
 }
