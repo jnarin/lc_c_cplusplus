9922b10843d1b89d4e33dbdf4e95bcc4428f52b9
jnarin <jnarin@users.noreply.github.com>
2023-06-21 20:52:36 +0530

    Create 1051. Height Checker.c

---
+++ 1051. Height Checker.c
@@ -0,0 +1,22 @@
+/*
+ * Link: https://leetcode.com/problems/height-checker/
+ * Problem: 1051. Height Checker
+ * */
+
+static int incr_cmp(void *a, void *b){
+    return *(int *)a - *(int *)b;
+}
+
+int heightChecker(int* heights, int heightsSize){
+    int sortedHeights[heightsSize];
+    memcpy(sortedHeights, heights, sizeof(sortedHeights));
+    qsort(sortedHeights, heightsSize, sizeof(int), incr_cmp);
+
+    int i, ans = 0;
+
+    for (i = 0; i < heightsSize; i++){
+        ans = heights[i] != sortedHeights[i] ? ans + 1 : ans;
+    }
+
+    return ans;
+}
