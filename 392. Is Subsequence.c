b7da04e980215727c0b40d1e837de0572da1ed61
jnarin <jnarin@users.noreply.github.com>
2023-09-22 09:39:51 +0530

    Add files via upload

--- 392. Is Subsequence.c
+++ 392. Is Subsequence.c
@@ -5,7 +5,11 @@ Probem: 392. Is Subsequence
 bool isSubsequence(char * s, char * t){
     int sLen = strlen(s), tLen = strlen(t), i = 0, j = 0;
     
-    while (i < sLen && j < tLen){
+    if (sLen > tLen){
+        return false;
+    }
+    
+    while (i < sLen && j < tLen && i <= j){
         if (s[i] != t[j]){
             j++;
             continue;
