4892d7f0a718fc9c11e14a3d7edb1ae62b32f8f0
jnarin <jnarin@users.noreply.github.com>
2023-09-25 15:58:42 +0530

    Add files via upload

--- 389. Find the Difference.c
+++ 389. Find the Difference.c
@@ -1,23 +1,20 @@
 /*
 Link: https://leetcode.com/problems/find-the-difference/
 Problem: 389. Find the Difference
+If the two strings are combined together, the problem is to 
+find the character whose count is odd.
 */
-#define MAX_ALPHA   (26)
-
 char findTheDifference(char * s, char * t){
-    int map[MAX_ALPHA] = {0}, lenS = strlen(s), lenT = strlen(t), i;
+    int lenS = strlen(s), lenT = strlen(t), i;
+    char ans = 0;
     
     for (i = 0; i < lenS; i++){
-        map[s[i] - 'a']++;
+        ans ^= s[i];
     }
     
     for (i = 0; i < lenT; i++){
-        if (map[t[i] - 'a'] <= 0){
-            return t[i];
-        }
-        
-        map[t[i] - 'a']--;
+        ans ^= t[i];
     }
     
-    return 0;
+    return ans;
 }
