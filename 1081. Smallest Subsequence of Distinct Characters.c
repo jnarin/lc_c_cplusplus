4ae23a69872c2621459a6d865f3d3bd0bbcb349b
jnarin <jnarin@users.noreply.github.com>
2023-09-26 09:42:23 +0530

    Add files via upload

---
+++ 1081. Smallest Subsequence of Distinct Characters.c
@@ -0,0 +1,34 @@
+/*
+Link: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+Problem: 1081. Smallest Subsequence of Distinct Characters
+*/
+#define MAX_ALPHA   (26)
+#define MAX_INPUT   (1000)
+
+char * smallestSubsequence(char * s){
+    char *ans = calloc(MAX_INPUT + 1, sizeof(char));
+    int index_map[MAX_ALPHA] = {0}, 
+        visit_map[MAX_ALPHA] = {0}, i,
+        sLen = strlen(s), ansIdx = 0;
+    
+    for (i = 0; i < sLen; i++){
+        index_map[s[i] - 'a'] = i;
+    }
+    
+    for (i = 0; i < sLen; i++){
+        if (visit_map[s[i] - 'a']){
+            continue;
+        }
+        
+        while (ansIdx && ans[ansIdx - 1] > s[i] 
+               && i <= index_map[ans[ansIdx - 1] - 'a']){
+            visit_map[ans[ansIdx - 1] - 'a'] = 0;
+            ansIdx--;
+        }
+        
+        ans[ansIdx++] = s[i];
+        visit_map[s[i] - 'a'] = 1;
+    }
+        
+    return ans;
+}
