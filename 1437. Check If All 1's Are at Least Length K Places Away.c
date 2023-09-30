a385b785862c971c33537fea7f5b28b940c4cefc
jnarin <jnarin@users.noreply.github.com>
2023-09-30 09:40:10 +0530

    Create 1437. Check If All 1's Are at Least Length K Places Away.c

---
+++ 1437. Check If All 1's Are at Least Length K Places Away.c
@@ -0,0 +1,31 @@
+/*
+ * Link: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+ * Problem: 1437. Check If All 1's Are at Least Length K Places Away
+ * */
+bool kLengthApart(int* nums, int numsSize, int k){
+    int i = 0, p = -1;
+
+    while (i < numsSize){
+        if (nums[i] != 0){
+            p = i;
+            i++;
+            break;
+        }
+
+        i++;
+    }
+
+    for (; i < numsSize; i++){
+        if (nums[i] != 1){
+            continue;
+        }
+
+        if (i - p <= k){
+            return false;
+        }
+
+        p = i;
+    }
+
+    return true;
+}
