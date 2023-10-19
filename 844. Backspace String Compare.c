473359ec8b54537d14937aadefcdd8a93eca996a
jnarin <jnarin@users.noreply.github.com>
2023-10-19 04:38:55 +0000

    Add files via upload

---
+++ 844. Backspace String Compare.c
@@ -0,0 +1,76 @@
+/*
+Problem: https://leetcode.com/problems/backspace-string-compare/
+Link: 844. Backspace String Compare
+*/
+
+#define CHECK_SKIP_CHAR(c) (((c) == '#') || ((c) == '*'))
+
+static void parseBS(char *s){
+    int sLen = strlen(s), i = sLen - 1, x;
+    
+    while (i >= 0){
+        if (s[i] == '#'){
+            x = i - 1;
+            while (x >= 0 && CHECK_SKIP_CHAR(s[x])){
+                x--;
+            }
+            
+            if (x >= 0){
+                s[x] = '*';
+            }
+        }
+        
+        i--;
+    }
+}
+
+static bool compareBS(char * s, char * t){
+    int sLen = strlen(s), tLen = strlen(t);
+    int i, j, a, b;
+    bool skipCheck = false;
+    
+    i = j = a = b = 0;
+    while (i < sLen || j < tLen){
+        if (CHECK_SKIP_CHAR(s[i])){
+            skipCheck = true;
+            i++;
+        }
+        
+        if (CHECK_SKIP_CHAR(t[j])){
+            skipCheck = true;
+            j++;
+        }
+        
+        if (skipCheck){
+            skipCheck = false;
+            continue;
+        }
+        
+        if (i < sLen && j < tLen && s[i] != t[j]){
+            return false;
+        }
+        
+        if (i < sLen){
+            i++;
+            a++;
+        }
+        
+        if (j < tLen){
+            j++;
+            b++;
+        }
+    }
+    
+    return a == b ? true : false;
+}
+
+bool backspaceCompare(char * s, char * t){
+    if (strcmp(s, t) == 0){
+        return true;
+    }
+    
+    parseBS(s);
+    parseBS(t);
+    
+    return compareBS(s, t);
+}
