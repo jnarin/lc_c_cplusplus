c8ba83443e52e93c0331fa5d4986b3e63ae8c552
jnarin <jnarin@users.noreply.github.com>
2024-02-20 11:57:16 +0530

    Add files via upload

--- 268. Missing Number.c
+++ 268. Missing Number.c
@@ -2,13 +2,17 @@
 Link: https://leetcode.com/problems/missing-number/
 Problem: 268. Missing Number
 */
-int missingNumber(int* nums, int numsSize){
+
+static int arraySum(int *nums, int numsSize) {
     int sum = 0, i;
-    for (i = 0; i < numsSize; i++){
+    
+    for (i = 0; i < numsSize; i++) {
         sum += nums[i];
     }
     
-    int t = (numsSize * (numsSize + 1)) / 2;
-    
-    return t - sum;
+    return sum;
+}
+
+int missingNumber(int* nums, int numsSize) {
+    return ((numsSize * (numsSize + 1)) >> 1) - arraySum(nums, numsSize);
 }
