95cd5506f0b42341d1474df2e97965b55d2c03d6
jnarin <jnarin@users.noreply.github.com>
2024-02-21 06:42:29 +0530

    Add files via upload

--- 201. Bitwise AND of Numbers Range.c
+++ 201. Bitwise AND of Numbers Range.c
@@ -2,15 +2,16 @@
 Link: https://leetcode.com/problems/bitwise-and-of-numbers-range/
 Problem: 201. Bitwise AND of Numbers Range
 */
-int rangeBitwiseAnd(int left, int right){
-    unsigned int i;
-    int ans = INT_MAX;
-    for (i = left; i <= right; i++){
-        ans &= i;
-        if (ans == 0){
-            break;
-        }
-    }
 
-    return ans;
+
+int rangeBitwiseAnd(int left, int right) {
+    int count = 0;
+    
+    while (left != right) {
+        left >>= 1;
+        right >>= 1;
+        count++;
+    }
+    
+    return left << count;    
 }
