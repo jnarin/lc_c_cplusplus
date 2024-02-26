d6061ab8acfd48db4fe0d2ae27c5c042c328d777
jnarin <jnarin@users.noreply.github.com>
2024-02-26 10:16:00 +0530

    Add files via upload

---
+++ 100. Same Tree.c
@@ -0,0 +1,28 @@
+/*
+Link: https://leetcode.com/problems/same-tree/
+Problem: 100. Same Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+    
+    if (p == NULL || q == NULL) {
+        return false;
+    }
+    
+    if (p->val != q->val) {
+        return false;
+    }
+    
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
