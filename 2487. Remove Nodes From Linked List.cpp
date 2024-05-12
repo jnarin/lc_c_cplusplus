96c3adfdeb9a3c5245f69112ecd5e4a46b26941c
jnarin <jnarin@users.noreply.github.com>
2024-05-12 07:04:09 +0530

    Create 2487. Remove Nodes From Linked List.cpp

---
+++ 2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem: 2487. Remove Nodes From Linked List
+ * Link: https://leetcode.com/problems/remove-nodes-from-linked-list/
+ * */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+ public:
+  ListNode* removeNodes(ListNode* head) {
+    if (head == nullptr)
+      return nullptr;
+    head->next = removeNodes(head->next);
+    return head->next != nullptr && head->val < head->next->val ? head->next
+                                                                : head;
+  }
+};
