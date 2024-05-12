4be1768a14bf269130e2a2b91badc25dbfc8edc6
jnarin <jnarin@users.noreply.github.com>
2024-05-12 07:05:41 +0530

    Create 2816. Double a Number Represented as a Linked List.cpp

---
+++ 2816. Double a Number Represented as a Linked List.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem: 2816. Double a Number Represented as a Linked List
+ * Link: https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/
+ * */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* doubleIt(ListNode* head) {
+    if (getCarry(head) == 1)
+      return new ListNode(1, head);
+    return head;
+  }
+
+ private:
+  int getCarry(ListNode* node) {
+    int val = node->val * 2;
+    if (node->next != nullptr)
+      val += getCarry(node->next);
+
+    node->val = val % 10;
+    return val / 10;
+  }
+};
