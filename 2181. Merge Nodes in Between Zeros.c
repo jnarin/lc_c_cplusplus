8ac0669a2069aced602a1caba6bfde9c29843af7
jnarin <jnarin@users.noreply.github.com>
2024-07-05 04:53:28 +0000

    Update 2181. Merge Nodes in Between Zeros.c

--- 2181. Merge Nodes in Between Zeros.c
+++ 2181. Merge Nodes in Between Zeros.c
@@ -10,97 +10,61 @@ Link: https://leetcode.com/problems/merge-nodes-in-between-zeros/
  *     struct ListNode *next;
  * };
  */
-
-static int countNodes(struct ListNode *head) {
-    struct ListNode *n = head;
-    int c = 0;
-    
-    while (n) {
-        c++;
-        n = n->next;
-    }
-    
-    return c;
-}
-
-static void lToA(struct ListNode *head, int *t) {
-    struct ListNode *n = head;
-    int idx = 0;
-    
-    while (n) {
-        t[idx++] = n->val;
-        n = n->next;
-    }
-}
-
-static int getSum(int *t, int l, int r) {
-    int sum = 0, i;
-    
-    for (i = l; i < r; i++) {
-        sum += t[i];
-    }
-    
-    return sum;
-}
-
 struct ListNode* mergeNodes(struct ListNode* head) {
-    int count = countNodes(head);
-    int t[count];
-    memset(t, 0, sizeof(t));
+    struct ListNode *l, *r;
     
-    lToA(head, t);
+    l = head;
     
-    int l = 0, r = 0, i;
-    
-    while (l < count && r < count) {
-        while (l < count) {
-            if (t[l] == 0) {
+    while (l) {
+        while (l) {
+            if (l->val == 0) {
                 break;
             }
             
-            l++;
+            l = l->next;
         }
         
-        r = l + 1;
-        while (r < count) {
-            if (t[r] == 0) {
+        if (l == NULL) {
+            break;
+        }
+        
+        int sum = 0;
+        
+        r = l->next;
+        while (r) {
+            if (r->val == 0) {
                 break;
             }
             
-            r++;
+            sum += r->val;
+            r->val = 0;
+            r = r->next;
         }
         
-        int sum = getSum(t, l, r);
-        t[l] = sum;
-        
-        for (i = l + 1; i < r && i < count; i++) {
-            t[i] = 0;
-        }
-         
-        l =  r;
-    }
-    
-    
-    struct ListNode *n = head, *p = NULL;
-    for (i = 0; i < count; i++) {
-        if (t[i] == 0) {
-            continue;
+        if (r == NULL) {
+            break;
         }
         
-        p = n;
-        n->val = t[i];
-        n = n->next;
+        l->val = sum;
+        l = r;
     }
     
-    
-    if (p) {
-        p->next = NULL;
-    }
+    struct ListNode *p = NULL, *n = head;
     
     while (n) {
-        p = n->next;
-        free(n);
-        n = p;
+        if (n->val == 0) {
+            if (p) {
+                struct ListNode *t = n;
+                p->next = n->next;
+                n = n->next;
+                free(t);
+                continue;
+            }
+        } else {
+            p = n;
+        }
+        
+        n = n->next;
     }
     
     return head;
