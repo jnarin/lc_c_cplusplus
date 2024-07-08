2cea83b7ba9017f5c8d6183f12a94b7a03465c19
jnarin <jnarin@users.noreply.github.com>
2024-07-08 15:36:02 +0000

    Update 3120. Count the Number of Special Characters I.c

--- 3120. Count the Number of Special Characters I.c
+++ 3120. Count the Number of Special Characters I.c
@@ -2,3 +2,19 @@
 Problem: 3120. Count the Number of Special Characters I
 Link: https://leetcode.com/problems/count-the-number-of-special-characters-i/
 */
+
+int numberOfSpecialChars(char* word) {
+    int len = strlen(word), map[128] = {0}, i, ans = 0;
+    
+    for (i = 0; i < len; i++) {
+        map[word[i]]++;
+    }
+    
+    for (i = 'A'; i <= 'Z'; i++) {
+        if (map[i]) {
+            ans += map[i - 'A' + 'a'] ? 1 : 0;
+        }
+    }
+    
+    return ans;
+}
