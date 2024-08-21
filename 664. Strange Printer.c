5c7a9ac67158ed85492a5a39354e0b4f3eccc3d5
jnarin <jnarin@users.noreply.github.com>
2024-08-21 03:22:03 +0000

    Update 664. Strange Printer.c

--- 664. Strange Printer.c
+++ 664. Strange Printer.c
@@ -9,43 +9,35 @@
     __typeof__(b) _b = (b);\
     _a < _b ? _a : _b;  })
 
-static int solve(int **dp, char *s, int i, int j){
+static int solve(int len, int dp[len][len], char *s, int i, int j){
     int k;
-
+    
     if (i > j){
         return 0;
     }
-
+    
     if (dp[i][j] > 0){
         return dp[i][j];
     }
-
-    dp[i][j] = solve(dp, s, i + 1, j) + 1;
-
+    
+    dp[i][j] = solve(len, dp, s, i + 1, j) + 1;
+    
     for (k = i + 1; k <= j; k++){
         if (s[i] == s[k]){
-            dp[i][j] = MINVAL(dp[i][j], solve(dp, s, i, k - 1) + solve(dp, s, k + 1, j));
+            dp[i][j] = MINVAL(dp[i][j], solve(len, dp, s, i, k - 1) + solve(len, dp, s, k + 1, j));
         }
     }
-
+    
     return dp[i][j];
 }
 
 int strangePrinter(char * s){
-    int len = strlen(s), **dp, i;
+    int len = strlen(s), dp[len][len], i;
     int ans;
-
-    dp = calloc(len, sizeof(int *));
-    for (i = 0; i < len; i++){
-        dp[i] = calloc(len, sizeof(int));
-    }
-
-    ans = solve(dp, s, 0, len - 1);
-
-    for (i = 0; i < len; i++){
-        free(dp[i]);
-    }
-    free(dp);
-
+    
+    memset(dp, 0, sizeof(dp));
+    
+    ans = solve(len, dp, s, 0, len - 1);
+    
     return ans;
 }
