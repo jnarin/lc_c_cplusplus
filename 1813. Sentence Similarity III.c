00305b33e99ea934cc3a01537c6606b9f9f843d3
jnarin <jnarin@users.noreply.github.com>
2024-10-06 10:08:44 +0530

    Update 1813. Sentence Similarity III.c

--- 1813. Sentence Similarity III.c
+++ 1813. Sentence Similarity III.c
@@ -5,93 +5,109 @@ Link: https://leetcode.com/problems/sentence-similarity-iii/
 
 #define MAXVAL(a, b) (((a) > (b)) ? (a) : (b))
 
-static int sentenceParse(char *s, int sLen, int *longestWordLen) {
-    int words = 1, i;
-    int longestLen = INT_MIN, p = 0;
+struct sentenceProps {
+    char *p;
+    int len;
+    int longestWordLen;
+    int numWords;
+    char **words;
+};
+
+static void sentenceParse(struct sentenceProps *s) {
+    int i, t = 0;
     
-    for (i = 0; i < sLen; i++) {
-        if (s[i] == ' ') {
-            *longestWordLen = MAXVAL(*longestWordLen, i - p);
-            p = i;
-            words++;
+    for (i = 0; i < s->len; i++) {
+        if (s->p[i] == ' ') {
+            s->longestWordLen = MAXVAL(s->longestWordLen, i - t);
+            t = i;
+            s->numWords++;
         }
     }
     
-    *longestWordLen = words == 1 ? sLen : MAXVAL(*longestWordLen, sLen - p - 1);
-    
-    return words;
+    s->longestWordLen = s->numWords == 1 ? s->len : MAXVAL(s->longestWordLen, s->len - t - 1);
 }
 
-static void extractWords(char *s, int sLen, char **words) {
-    int i, p = 0, word = 0;
-    for (i = 0; i < sLen; i++) {
-        if (s[i] == ' ') {
-            words[word][i - p] = '\0';
-            p = i + 1;
+static void extractWords(struct sentenceProps *s) {
+    int i, t = 0, word = 0;
+
+    for (i = 0; i < s->len; i++) {
+        if (s->p[i] == ' ') {
+            s->words[word][i - t] = '\0';
+            t = i + 1;
             word++;
         } else {
-            words[word][i - p] = s[i];
+            s->words[word][i - t] = s->p[i];
         }
     }
-    words[word][sLen - p] = '\0';
+
+    s->words[word][s->len - t] = '\0';
 }
 
 bool areSentencesSimilar(char* sentence1, char* sentence2) {
-    int len1 = strlen(sentence1), len2 = strlen(sentence2);
-    if (len1 == len2) {
+    struct sentenceProps s1 = {0}, s2 = {0};
+
+    s1.p = sentence1;
+    s2.p = sentence2;
+
+    s1.len = strlen(sentence1);
+    s2.len = strlen(sentence2);
+
+    if (s1.len == s2.len) {
         return strcmp(sentence1, sentence2) == 0;
     }
     
-    if (len1 == 0 || len2 == 0) {
+    if (s1.len == 0 || s2.len == 0) {
         return true;
     }
-        
-    int longestWord1 = 0, longestWord2 = 0;
-    int numWords1 = sentenceParse(sentence1, len1, &longestWord1), numWords2 = sentenceParse(sentence2, len2, &longestWord2);
 
-    if (numWords1 > numWords2) {
+    s1.numWords = s2.numWords = 1;
+    s1.longestWordLen = s2.longestWordLen = INT_MIN;
+
+    sentenceParse(&s1);
+    sentenceParse(&s2);
+
+    if (s1.numWords > s2.numWords) {
         return areSentencesSimilar(sentence2, sentence1);
     }
     
-    char **words1, **words2;
-    words1 = calloc(numWords1, sizeof(char *));
-    words2 = calloc(numWords2, sizeof(char *));
+    s1.words = calloc(s1.numWords, sizeof(char *));
+    s2.words = calloc(s2.numWords, sizeof(char *));
     
     int i;
     
-    for (i = 0; i < numWords1; i++) {
-        words1[i] = calloc(longestWord1 + 1, sizeof(char));
+    for (i = 0; i < s1.numWords; i++) {
+        s1.words[i] = calloc(s1.longestWordLen + 1, sizeof(char));
     }
     
-    for (i = 0; i < numWords2; i++) {
-        words2[i] = calloc(longestWord2 + 1, sizeof(char));
+    for (i = 0; i < s2.numWords; i++) {
+        s2.words[i] = calloc(s2.longestWordLen + 1, sizeof(char));
     }
     
-    extractWords(sentence1, len1, words1);
-    extractWords(sentence2, len2, words2);
+    extractWords(&s1);
+    extractWords(&s2);
 
     bool ans;
     i = 0;
     
-    while (i < numWords1 && strcmp(words1[i], words2[i]) == 0) {
+    while (i < s1.numWords && strcmp(s1.words[i], s2.words[i]) == 0) {
         i++;
     }
 
-    while (i < numWords1 && strcmp(words1[i], words2[i + numWords2 - numWords1]) == 0) {
+    while (i < s1.numWords && strcmp(s1.words[i], s2.words[i + s2.numWords - s1.numWords]) == 0) {
         i++;
     }
     
-    ans = i == numWords1;
+    ans = i == s1.numWords;
     
-    for (i = 0; i < numWords1; i++) {
-        free(words1[i]);
+    for (i = 0; i < s1.numWords; i++) {
+        free(s1.words[i]);
     }
-    free(words1);
+    free(s1.words);
     
-    for (i = 0; i < numWords2; i++) {
-        free(words2[i]);
+    for (i = 0; i < s2.numWords; i++) {
+        free(s2.words[i]);
     }
-    free(words2);
+    free(s2.words);
     
     return ans;
 }
