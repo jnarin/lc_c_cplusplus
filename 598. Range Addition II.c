1bc932e4d864c2351f1481dc3659827666997dc5
jnarin <jnarin@users.noreply.github.com>
2024-10-23 10:00:42 +0000

    Update 598. Range Addition II.c

--- 598. Range Addition II.c
+++ 598. Range Addition II.c
@@ -3,12 +3,14 @@ Problem: 598. Range Addition II
 Link: https://leetcode.com/problems/range-addition-ii/
 */
 
+#define MINVAL(a, b) (((a) < (b)) ? (a) : (b))
+
 int maxCount(int m, int n, int** ops, int opsSize, int* opsColSize){
     int minRow = m, minCol = n, i;
 
     for(i = 0; i < opsSize; i++){
-        minRow = ops[i][0] < minRow ? ops[i][0] : minRow;
-        minCol = ops[i][1] < minCol ? ops[i][1] : minCol;
+        minRow = MINVAL(ops[i][0], minRow);
+        minCol = MINVAL(ops[i][1], minCol);
     }
 
     return minRow * minCol;
