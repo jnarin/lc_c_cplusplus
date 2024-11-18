b853e112b8b6152e0020dec67f157686d07827f3
jnarin <jnarin@users.noreply.github.com>
2024-11-18 04:19:16 +0000

    Update 1652. Defuse the Bomb.c

--- 1652. Defuse the Bomb.c
+++ 1652. Defuse the Bomb.c
@@ -7,29 +7,27 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* decrypt(int* code, int codeSize, int k, int* returnSize) {
-    int *ans = calloc(codeSize, sizeof(int));
+    int i, *ans = calloc(codeSize, sizeof(int)), j;
+    
     *returnSize = codeSize;
-
-    if (k != 0) {
-        int sum = 0, i;
-        int start = k > 0 ? 1 : codeSize + k;
-        int end = k > 0 ? k : codeSize - 1;
-
-        for (i = start; i <= end; i++) {
-            sum += code[i];
-        }
-
-        for (i = 0; i < codeSize; i++) {
-            ans[i] = sum;
-            sum -= code[start % codeSize];
-            start++;
-
-            end++;
-            sum += code[end % codeSize];
-        }
-
+    
+    if (k == 0) {
         return ans;
     }
-
+    
+    for (i = 0; i < codeSize; i++) {
+        if (k > 0) {
+            for (j = i + 1; j < i + k + 1; j++) {
+                ans[i] += code[j % codeSize];
+                
+            }
+        } else {
+            for (j = i + k; j < i; j++) {
+                ans[i] += code[(j + codeSize) % codeSize];
+            }
+        }
+    }
+    
+    
     return ans;
 }
