003c19a5164adb7fc0faaa1c1a7beae86a1bfedf
jnarin <jnarin@users.noreply.github.com>
2025-01-13 04:52:50 +0000

    Create 3223. Minimum Length of String After Operations.c

---
+++ 3223. Minimum Length of String After Operations.c
@@ -0,0 +1,23 @@
+/*
+Problem: 3223. Minimum Length of String After Operations
+Link: https://leetcode.com/problems/minimum-length-of-string-after-operations/description/
+*/
+
+
+int minimumLength(char* s) {
+    int map[26] = {0}, len = strlen(s), i, ans = 0;
+
+    for (i = 0; i < len; i++) {
+        map[s[i] - 'a']++;
+    }
+
+    for (i = 0; i < 26; i++) {
+        map[i] = map[i] > 2 ? ((map[i] & 1) ? 1 : 2) : map[i];
+    }
+
+    for (i = 0; i < 26; i++) {
+        ans += map[i];
+    }
+
+    return ans;
+}
