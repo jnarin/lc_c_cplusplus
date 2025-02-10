eb1934f4d8f4d785049c7e292cea129c33e921c2
jnarin <jnarin@users.noreply.github.com>
2025-02-10 11:01:52 +0530

    Update 3174. Clear Digits.c

--- 3174. Clear Digits.c
+++ 3174. Clear Digits.c
@@ -4,8 +4,8 @@ Link: https://leetcode.com/problems/clear-digits/
 */
 
 char* clearDigits(char* s) {
-    int len = strlen(s), i = 0;
-    char *ans = calloc(len + 1, sizeof(char)), idx = 0;
+    int len = strlen(s), i = 0, idx = 0;
+    char *ans = calloc(len + 1, sizeof(char));
     
     while (i < len) {
         if (s[i] >= '0' && s[i] <= '9' && idx) {
@@ -20,5 +20,4 @@ char* clearDigits(char* s) {
     ans[idx] = '\0';
     
     return ans;
-    
 }
